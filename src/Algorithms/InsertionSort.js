2aabcf0cfdc44eca32b5e67aa7b8727728761a76
kiranml1 <kirankumar.amruthaluri@gmail.com>
2016-11-16 22:43:51 +0530

    [REFACTOR](Insertionsort.js, seqOrderEnum.js): update insertion sort and add sequential enum data to execute algorithm

--- src/Algorithms/InsertionSort.js
+++ src/Algorithms/InsertionSort.js
@@ -1,38 +1,19 @@
 import Algorithm from '../Abstractions/Algorithm';
+import ORDER from '../register/seqOrderEnum';
 
 export default class InsertionSort extends Algorithm {
-
-  constructor() {
-    super();
-    this.orders = new Map();
-    this.setOrders();
-  }
-
-  setOrders() {
-    this.orders.set('ASC', 'asc');
-    this.orders.set('DESC', 'desc');
-  }
-
-  set order(order) {
-    this._order = this.orders.get(order);
-  }
-
-  get order() {
-    return this._order;
-  }
 
   execute(input, order) {
     const sortedSet = input;
     for (let i = 1, j, key; i < sortedSet.length; i += 1) {
       key = sortedSet[i];
       j = i - 1;
-      this.order = order;
-      if (this.orders.get('ASC') === this.order) {
+      if (ORDER.ARRANGE_ORDER.INCREASING_ORDER === order) {
         while (j >= 0 && sortedSet[j] > key) {
           sortedSet[j + 1] = sortedSet[j];
           j -= 1;
         }
-      } else if (this.orders.get('DESC') === this.order) {
+      } else if (ORDER.ARRANGE_ORDER.DECREASING_ORDER === order) {
         while (j >= 0 && sortedSet[j] < key) {
           sortedSet[j + 1] = sortedSet[j];
           j -= 1;
---
+++ src/register/seqOrderEnum.js
@@ -0,0 +1,6 @@
+export default {
+  ARRANGE_ORDER: {
+    INCREASING_ORDER: 'ASC',
+    DECREASING_ORDER: 'DESC',
+  },
+};
