6da667bdb64777ea23b03ae78c975872067db700
kiranml1 <kirankumar.amruthaluri@gmail.com>
2016-11-24 15:19:04 +0530

    [TEST](LinearSearch.js): Add unit tests for linear search with number, string and boolean values

--- tests/Algorithms/search/LinearSearch.js
+++ tests/Algorithms/search/LinearSearch.js
@@ -14,13 +14,38 @@ describe('linear search algorithm\'s test suite', () => {
     expect(linearSearch.name).not.to.eql('LinearSearchAlgorithm');
   });
 
-  it('expect the given value exists in the collection', () => {
+  it('expect the given value of type number exists in the collection', () => {
     expect(linearSearch.execute([5, 2, 4, 6, 1, 3], 3)).to.eql(true);
     expect(linearSearch.execute([5, 2, 4, 6, 1, 3], 5)).to.eql(true);
   });
 
-  it('expect not the given value exists in the collection', () => {
+  it('expect not the given value of type number exists in the collection', () => {
     expect(linearSearch.execute([5, 2, 4, 6, 1, 3], 8)).not.to.eql(true);
     expect(linearSearch.execute([5, 2, 4, 6, 1, 3], 100)).not.to.eql(true);
+  });
+
+  it('expect the given value of type string exists in the collection', () => {
+    expect(linearSearch.execute(['5', '2', '4', '6', '1', '3'], '3')).to.eql(true);
+    expect(linearSearch.execute(['5', '2', '4', '6', '1', '3'], '5')).to.eql(true);
+  });
+
+  it('expect note the given value of type string exists in the collection', () => {
+    expect(linearSearch.execute(['5', '2', '4', '6', '1', '3'], 3)).not.to.eql(true);
+    expect(linearSearch.execute(['5', '2', '4', '6', '1', '3'], 5)).not.to.eql(true);
+  });
+
+  it('expect not the given value of type number exists in the collection', () => {
+    expect(linearSearch.execute(['5', '2', '4', '6', '1', '3'], 8)).not.to.eql(true);
+    expect(linearSearch.execute(['5', '2', '4', '6', '1', '3'], 100)).not.to.eql(true);
+  });
+
+  it('expect the given value of type boolean exists in the collection', () => {
+    expect(linearSearch.execute([true, false, true, false], true)).to.eql(true);
+    expect(linearSearch.execute([true, false, true, false], true)).to.eql(true);
+  });
+
+  it('expect not the given value of type boolean exists in the collection', () => {
+    expect(linearSearch.execute([true, false, true, false], 'true')).not.to.eql(true);
+    expect(linearSearch.execute([true, false, true, false], 'false')).not.to.eql(true);
   });
 });
