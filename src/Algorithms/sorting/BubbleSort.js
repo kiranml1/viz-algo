0930dabf10a78b14b352238f2768abae446507a2
kiranml1 <kirankumar.amruthaluri@gmail.com>
2016-11-26 14:43:14 +0530

    [FEAT](Bubblesort.js): Add bubble sort algorithm implementation and unit tests

---
+++ src/Algorithms/sorting/BubbleSort.js
@@ -0,0 +1,37 @@
+import Algorithm from '../../Abstractions/Algorithm';
+import ORDER from '../../register/seqOrderEnum';
+
+/**
+ * Bubble sort algorithm implementation - sorts a collection/array values into sorted sequence
+ * of decreasing or increasing series from unsorted random series of provided input
+ */
+export default class BubbleSort extends Algorithm {
+
+  /**
+   * Sorts a collection/array values into sorted sequence of decreasing or increasing series from
+   * unsorted random series of provided input and preferred order
+   * @param {Array} input  Collection of random series with unsorted series
+   * @param {string} order  Preferred order input - 'ASC' (ascending) or 'DESC (descending)
+   * @returns {Array} Sorted collection of given order from input
+   */
+  execute(input, order) {
+    const sortedSet = input;
+    for (let i = sortedSet.length - 1; i >= 0; i -= 1) {
+      for (let j = 1; j <= i; j += 1) {
+        if (ORDER.ARRANGE_ORDER.INCREASING_ORDER === order && sortedSet[j - 1] > sortedSet[j]) {
+          const temp = sortedSet[j - 1];
+          sortedSet[j - 1] = sortedSet[j];
+          sortedSet[j] = temp;
+        } else if (ORDER.ARRANGE_ORDER.DECREASING_ORDER === order) {
+          if (sortedSet[j - 1] < sortedSet[j]) {
+            const temp = sortedSet[j - 1];
+            sortedSet[j - 1] = sortedSet[j];
+            sortedSet[j] = temp;
+          }
+        }
+      }
+    }
+    return sortedSet;
+  }
+
+}
---
+++ tests/Algorithms/sorting/BubbleSort.js
@@ -0,0 +1,39 @@
+import { expect } from 'chai';
+import BubbleSort from '../../../src/Algorithms/sorting/BubbleSort';
+
+describe('insertion sort algorithm\'s test suite', () => {
+
+  let algorithmInstance;
+
+  beforeEach(() => {
+    algorithmInstance = new BubbleSort();
+  });
+
+  it('expect class name of insertion sort algorithm in instance', () => {
+    expect(algorithmInstance.name).to.eql('BubbleSort');
+  });
+
+  it('expect ascending sequence after insertion sort for the input of numbers', () => {
+    const input = [5, 2, 4, 6, 1, 3];
+    const expected = [ 1, 2, 3, 4, 5, 6 ];
+    expect(algorithmInstance.execute(input, 'ASC')).to.eql(expected);
+  });
+
+  it('expect ascending sequence after insertion sort for the input of numbers', () => {
+    const input = [41, 59, 26, 41, 58];
+    const expected = [ 26, 41, 41, 58, 59];
+    expect(algorithmInstance.execute(input, 'ASC')).to.eql(expected);
+  });
+
+  it('expect descending sequence after insertion sort for the input of numbers', () => {
+    const input = [5, 2, 4, 6, 1, 3];
+    const expected = [ 6, 5, 4, 3, 2, 1];
+    expect(algorithmInstance.execute(input, 'DESC')).to.eql(expected);
+  });
+
+  it('expect descending sequence after insertion sort for the input of numbers', () => {
+    const input = [41, 59, 26, 41, 58];
+    const expected = [ 59, 58, 41, 41, 26 ];
+    expect(algorithmInstance.execute(input, 'DESC')).to.eql(expected);
+  });
+});
