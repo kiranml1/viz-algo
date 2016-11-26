d7495748da7af39914a339f09a704e78fb0ff406
kiranml1 <kirankumar.amruthaluri@gmail.com>
2016-11-26 14:45:22 +0530

    [TEST](Bubblesort.js): Refactor bubble sort unit tests with correct sorting name

--- tests/Algorithms/sorting/BubbleSort.js
+++ tests/Algorithms/sorting/BubbleSort.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import BubbleSort from '../../../src/Algorithms/sorting/BubbleSort';
 
-describe('insertion sort algorithm\'s test suite', () => {
+describe('bubble sort algorithm\'s test suite', () => {
 
   let algorithmInstance;
 
@@ -9,29 +9,29 @@ describe('insertion sort algorithm\'s test suite', () => {
     algorithmInstance = new BubbleSort();
   });
 
-  it('expect class name of insertion sort algorithm in instance', () => {
+  it('expect class name of bubble sort algorithm in instance', () => {
     expect(algorithmInstance.name).to.eql('BubbleSort');
   });
 
-  it('expect ascending sequence after insertion sort for the input of numbers', () => {
+  it('expect ascending sequence after bubble sort for the input of numbers', () => {
     const input = [5, 2, 4, 6, 1, 3];
     const expected = [ 1, 2, 3, 4, 5, 6 ];
     expect(algorithmInstance.execute(input, 'ASC')).to.eql(expected);
   });
 
-  it('expect ascending sequence after insertion sort for the input of numbers', () => {
+  it('expect ascending sequence after bubble sort for the input of numbers', () => {
     const input = [41, 59, 26, 41, 58];
     const expected = [ 26, 41, 41, 58, 59];
     expect(algorithmInstance.execute(input, 'ASC')).to.eql(expected);
   });
 
-  it('expect descending sequence after insertion sort for the input of numbers', () => {
+  it('expect descending sequence after bubble sort for the input of numbers', () => {
     const input = [5, 2, 4, 6, 1, 3];
     const expected = [ 6, 5, 4, 3, 2, 1];
     expect(algorithmInstance.execute(input, 'DESC')).to.eql(expected);
   });
 
-  it('expect descending sequence after insertion sort for the input of numbers', () => {
+  it('expect descending sequence after bubble sort for the input of numbers', () => {
     const input = [41, 59, 26, 41, 58];
     const expected = [ 59, 58, 41, 41, 26 ];
     expect(algorithmInstance.execute(input, 'DESC')).to.eql(expected);
